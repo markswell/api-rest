5f5d77290bb1670c4a20ad6d2eaf3b325161d86f
Markswell Menezes <markswellmenezes@outlook.com>
2018-04-02 19:42:31 -0300

    conectando ao postgres

---
+++ src/main/java/com/markswell/apirest/ApiRestApplication.java
@@ -0,0 +1,15 @@
+package com.markswell.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@SpringBootApplication
+public class ApiRestApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestApplication.class, args);
+	}
+}
